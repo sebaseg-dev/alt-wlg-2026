db804de4d8ee6776d313c5ffb6719b8b9c3b6a88
Sebastien Seguin <80358666+sebaseg-dev@users.noreply.github.com>
2026-05-26 11:23:42 +0200

    init astro + pwa plugin + draft pwa manifest and icons

---
+++ astro/astro.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+import AstroPWA from '@vite-pwa/astro';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        AstroPWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'alt wlg 2026',
+                short_name: 'alt wlg 26',
+                description: 'Timetable alternative et accessible du festival WeLoveGreen 2026',
+                theme_color: '#111111',
+                background_color: '#111111',
+                display: 'standalone',
+                orientation: 'portrait',
+                icons: [
+                    {
+                        src: 'icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,svg,png,ico,txt}'],
+                navigateFallback: '/'
+            }
+        }),
+    ],
+});
