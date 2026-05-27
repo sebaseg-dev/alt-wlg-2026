15bed4281f54fd2ec261fe949da4b31b86d671d6
Sebastien Seguin <80358666+sebaseg-dev@users.noreply.github.com>
2026-05-27 12:58:27 +0200

    add style

--- astro/src/constants.ts
+++ astro/src/constants.ts
@@ -1,1 +1,12 @@
 export const LATEST_CLOSING_HOUR = 3;
+
+export const COLORS = {
+    "DARK_POINT": "#424242",
+    "LIGHT_POINT": "#fbf9f8",
+    "ACCENT": "#4CAF50",
+    "SCENE_0_PLAIN": "#DBFE49",
+    "SCENE_1_PLAIN": "#66E000",
+    "SCENE_2_PLAIN": "#45B6FE",
+    "SCENE_3_PLAIN": "#A884FE",
+    "SCENE_4_PLAIN": "#FE6B03",
+};
